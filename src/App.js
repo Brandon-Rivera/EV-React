6f0e9aded1a2e067a3743bc58d33b65e49bd3d60
Brandon-Rivera <A01424338@tec.mx>
2022-09-29 15:59:02 -0500

    Agregar archivos base

--- src/App.js
+++ src/App.js
@@ -4,22 +4,33 @@ import React from 'react'
 // import { BrowserRouter, Routes, Route, Link} from 'react-router-dom'
 // import LoginForm from './components/LoginForm';
 // import RegisterForm from './components/RegisterForm';
-// import Layout from './views/Layout';
 
+// import Layout from './views/Layout';
 // import Home from './components/Home';
 // import Cursos from './components/Cursos';
 // import Biologia from './components/Biologia';
 // import BProcesos from './components/BProcesos';
 // import Estadisticas from './components/Estadisticas';
 
-function App() {
-  return (
-    <div style={{ 
-      backgroundImage: `url("https://via.placeholder.com/500")` 
-    }}>
-      Hello World
-    </div>
-  );
+
+const App = () => {
+  return(
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout/>}>
+          <Route index 
+                  element={<Home/>}></Route>
+          <Route path='login' element={<LoginForm/>}></Route>
+          <Route path='register' element={<RegisterForm/>}></Route>
+          <Route path='cursos' element={<Cursos/>}></Route>
+          <Route path='biologia' element={<Biologia/>}></Route>
+          <Route path='bprocesos' element={<BProcesos/>}></Route>
+          <Route path='estadisticas' element={<Estadisticas/>}></Route>
+          <Route path='descarga' element={<Link to="https://www.google.com"/>}></Route>
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  ); 
 }
 
 export default App;

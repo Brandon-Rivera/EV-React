20418d4266346eb815bc2af3bdcde91405e3c22a
Brandon-Rivera <A01424338@tec.mx>
2022-09-30 14:02:09 -0500

    Reinicio

--- src/App.js
+++ src/App.js
@@ -1,34 +1,25 @@
+import logo from './logo.svg';
 import './App.css';
-import React from 'react'
 
-import { BrowserRouter, Routes, Route, Link} from 'react-router-dom'
-import LoginForm from './components/LoginForm';
-import RegisterForm from './components/RegisterForm';
-
-import Layout from './views/Layout';
-import Home from './components/Home';
-// import Cursos from './components/Cursos';
-// import Biologia from './components/Biologia';
-// import BProcesos from './components/BProcesos';
-// import Estadisticas from './components/Estadisticas';
-
-
-const App = () => {
-  return(
-    <div> app </div>
-    // <BrowserRouter>
-    //   <Routes>
-    //     <Route path="/" element={<Layout/>}>
-    //       <Route index 
-    //               element={<Home/>}></Route>
-    //       <Route path='login' element={<LoginForm/>}></Route>
-    //       <Route path='register' element={<RegisterForm/>}></Route>
-    //       {/* <Route path='cursos' element={<Cursos/>}></Route>
-    //       <Route path='descarga' element={<Link to="https://www.google.com"/>}></Route> */}
-    //     </Route>
-    //   </Routes>
-    // </BrowserRouter>
-  ); 
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;

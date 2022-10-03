bd5608eec701b94dac744fb31c973567159f69ef
Brandon-Rivera <A01424338@tec.mx>
2022-10-03 11:32:48 -0500

    Agregar Navbar simple

--- src/components/Navbar/NavbarElements.js
+++ src/components/Navbar/NavbarElements.js
@@ -9,7 +9,7 @@ export const Nav = styled.nav`
     justify-content: space-between;
     padding: 0.5rem calc((100vw - 1000px) / 2);
     z-index: 10;
-`
+`;
 
 export const NavLink = styled(Link)`
     color: #fff;
@@ -23,7 +23,7 @@ export const NavLink = styled(Link)`
     &.active {
         color: #15cdfc
     }
-`
+`;
 
 export const Bars = styled(FaBars)`
     display: none;
@@ -38,7 +38,42 @@ export const Bars = styled(FaBars)`
         font-size: 1.8rem;
         cursor: pointer;
     }
-`
+`;
 
 export const NavMenu = styled.div`
-`
+    display: flex;
+    align-items: center;
+    margin-right: -24px;
+
+    @media screen and (max-width: 768px) {
+        display: none;
+    }
+`;
+
+export const NavBtn = styled.nav`
+    display: flex; 
+    align-items: center;
+    margin-right: 24px;
+
+    @media screen and (max-width: 768px) {
+        display: none;
+    }
+`;
+
+export const NavBtnLink = styled(Link)`
+    border-radius: 4px;
+    background: #256ce1;
+    padding: 10px 22px;
+    color: #fff;
+    border: none;
+    outline; none;
+    cursor: pointer;
+    transition: all 0.2s ease-in-out;
+    text-decoration: none;
+
+    &:hover {
+        transition: all 0.2s ease-in-out;
+        background: #fff;
+        color: #010606;
+    }
+`;
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,5 +1,6 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom';
+import React from 'react';
+// import { NavLink } from 'react-router-dom';
+import {Nav, Bars, NavMenu, NavLink, NavBtn, NavBtnLink} from './NavbarElements';
 
 const Navbar = () => {
   return (

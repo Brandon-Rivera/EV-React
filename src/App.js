31670b40a10ac68a366e1f6ef37073563b7d40b3
Brandon-Rivera <A01424338@itesm.mx>
2022-10-05 10:12:38 -0500

    Agregar logos y registro

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Insumos from './pages/insumos';
 import Detallado from './pages/detallado';
 import Registro from './pages/registroSesion';
 import Inicio from './pages/inicioSesion';
+import Form from './pages/Form';
 
 
 
@@ -20,7 +21,7 @@ function App() {
         <Route path="/informe-general" element={<General/>}></Route>
         <Route path="/insumos" element={<Insumos/>}></Route>
         <Route path="/reporte-detallado" element={<Detallado/>}></Route>
-        <Route path="/registrar-sesion" element={<Registro/>}></Route>
+        <Route path="/registrar-sesion" element={<Form/>}></Route>
         <Route path="/iniciar-sesion" element={<Inicio/>}></Route>
       </Routes>
     </Router>

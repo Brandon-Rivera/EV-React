499baafa4311a5bd3758e449546a249b2cc2e470
Brandon-Rivera <A01424338@tec.mx>
2022-10-06 18:11:39 -0500

    Agregar modal

--- src/pages/FormSignin.js
+++ src/pages/FormSignin.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import validate from './validateInfo2';
 import useForm from './useForm2';
+import ErrorModal from './ErrorModal';
 import './Form.css';
 
 const FormSignup = ({ submitForm }) => {
@@ -46,6 +47,7 @@ const FormSignup = ({ submitForm }) => {
         ¿No tienes una cuenta? Regístrate <a href='registrar-sesion'>aquí</a>
         </span>
       </form>
+      <ErrorModal show={modalShow} title='Error master!' message='Usuario o contraseña incorrectos' onHide={() => setModalShow(false)}/>
     </div>
   );
 };

7bc93548aa63497b9f4626b20114cb7ee5383c44
Frank <franciscotame0@gmail.com>
2022-10-06 19:44:49 -0500

    Background image y table headers

--- src/pages/insumos.js
+++ src/pages/insumos.js
@@ -11,75 +11,75 @@ const Insumos = () => {
             display: "block",
             height: "350px",
             overflow: "auto",
-            width: "60%"
+            width: "55%"
           }}>
         <thead>
           <tr>
-            <th>Beneficiario</th>
-            <th>Alimento 1</th>
-            <th>Alimento 2</th>
-            <th>Alimento 3</th>
-            <th>Alimento 4</th>
-            <th>Paquete</th>
-            <th>Ubicación</th>
+            <th style={{ position: "sticky", top: 0}}>Beneficiario</th>
+            <th style={{ position: "sticky", top: 0}}>Alimento 1</th>
+            <th style={{ position: "sticky", top: 0}}>Alimento 2</th>
+            <th style={{ position: "sticky", top: 0}}>Alimento 3</th>
+            <th style={{ position: "sticky", top: 0}}>Alimento 4</th>
+            <th style={{ position: "sticky", top: 0}}>Paquete</th>
+            <th style={{ position: "sticky", top: 0}}>Ubicación</th>
           </tr>
         </thead>
-        <tbody>
-          <tr>
-            <td>Familia Chavéz</td>
-            <td>marymin@gmail.com</td>
-            <td>7771247893</td>
-            <td>4</td>
-            <td>00070531</td>
-            <td><img src="assets/Plato.png" alt="" width="60px" height="60px"/></td>
-            <td><img src="assets/Mundo.png" alt="" width="60px" height="60px"/></td>
-          </tr>
-          <tr>
-            <td>Familia Rivera</td>
-            <td>volodyapavelev@gmail.com</td>
-            <td>7772680483</td>
-            <td>5</td>
-            <td>00070598</td>
-            <td><img src="assets/Plato.png" alt="" width="60px" height="60px"/></td>
-            <td><img src="assets/Mundo.png" alt="" width="60px" height="60px"/></td>
-          </tr>
-          <tr>
-            <td>Familia Sierra</td>
-            <td>atbatb1@gmail.com</td>
-            <td>7771104762</td>
-            <td>4</td>
-            <td>00070511</td>
-            <td><img src="assets/Plato.png" alt="" width="60px" height="60px"/></td>
-            <td><img src="assets/Mundo.png" alt="" width="60px" height="60px"/></td>
-          </tr>
-          <tr>
-            <td>Familia Jimenéz</td>
-            <td>mcal69@gmail.com</td>
-            <td>7771036588</td>
-            <td>4</td>
-            <td>00070512</td>
-            <td><img src="assets/Plato.png" alt="" width="60px" height="60px"/></td>
-            <td><img src="assets/Mundo.png" alt="" width="60px" height="60px"/></td>
-          </tr>
-          <tr>
-            <td>Familia González</td>
-            <td>junadelvie@gmail.com</td>
-            <td>7777192754</td>
-            <td>5</td>
-            <td>00070514</td>
-            <td><img src="assets/Plato.png" alt="" width="60px" height="60px"/></td>
-            <td><img src="assets/Mundo.png" alt="" width="60px" height="60px"/></td>
-          </tr>
-          <tr>
-            <td>Familia Hernández</td>
-            <td>bktoshiro@gmail.com</td>
-            <td>7771039456</td>
-            <td>4</td>
-            <td>00070523</td>
-            <td><img src="assets/Plato.png" alt="" width="60px" height="60px"/></td>
-            <td><img src="assets/Mundo.png" alt="" width="60px" height="60px"/></td>
-          </tr>
-        </tbody>
+          <tbody>
+            <tr>
+              <td>Familia Chavéz</td>
+              <td>1 kg de arroz</td>
+              <td>1 kg de frijol</td>
+              <td>1 kg de lenteja</td>
+              <td>1 kg de pollo</td>
+              <td><img src="assets/Plato.png" alt="" width="60px" height="60px"/></td>
+              <td><img src="assets/Mundo.png" alt="" width="60px" height="60px"/></td>
+            </tr>
+            <tr>
+              <td>Familia Rivera</td>
+              <td>1 kg de arroz</td>
+              <td>1 kg de frijol</td>
+              <td>1 kg de lenteja</td>
+              <td>1 kg de pollo</td>
+              <td><img src="assets/Plato.png" alt="" width="60px" height="60px"/></td>
+              <td><img src="assets/Mundo.png" alt="" width="60px" height="60px"/></td>
+            </tr>
+            <tr>
+              <td>Familia Sierra</td>
+              <td>1 kg de arroz</td>
+              <td>1 kg de frijol</td>
+              <td>1 kg de lenteja</td>
+              <td>1 kg de pollo</td>
+              <td><img src="assets/Plato.png" alt="" width="60px" height="60px"/></td>
+              <td><img src="assets/Mundo.png" alt="" width="60px" height="60px"/></td>
+            </tr>
+            <tr>
+              <td>Familia Jimenéz</td>
+              <td>1 kg de arroz</td>
+              <td>1 kg de frijol</td>
+              <td>1 kg de lenteja</td>
+              <td>1 kg de pollo</td>
+              <td><img src="assets/Plato.png" alt="" width="60px" height="60px"/></td>
+              <td><img src="assets/Mundo.png" alt="" width="60px" height="60px"/></td>
+            </tr>
+            <tr>
+              <td>Familia González</td>
+              <td>1 kg de arroz</td>
+              <td>1 kg de frijol</td>
+              <td>1 kg de lenteja</td>
+              <td>1 kg de pollo</td>
+              <td><img src="assets/Plato.png" alt="" width="60px" height="60px"/></td>
+              <td><img src="assets/Mundo.png" alt="" width="60px" height="60px"/></td>
+            </tr>
+            <tr>
+              <td>Familia Hernández</td>
+              <td>1 kg de arroz</td>
+              <td>1 kg de frijol</td>
+              <td>1 kg de lenteja</td>
+              <td>1 kg de pollo</td>
+              <td><img src="assets/Plato.png" alt="" width="60px" height="60px"/></td>
+              <td><img src="assets/Mundo.png" alt="" width="60px" height="60px"/></td>
+            </tr>
+          </tbody>
       </table>
     </div>
     
---
+++ src/pages/paquete.js
@@ -0,0 +1,6 @@
+import React from "react";
+import './paquete.css';
+
+const Paquete = () => {
+
+}

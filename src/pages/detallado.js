0a0d1b121ba4fbff57df32bd64ba4a317d190e41
Brandon-Rivera <A01424338@tec.mx>
2022-10-10 13:45:41 -0500

    CSS detallado

--- src/pages/detallado.js
+++ src/pages/detallado.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import './detallado.css';
 
 const Detallado = () => {
   return (
     <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', height:'90vh'}}>
-        <h1>Reporte Detallado</h1>
+        <h1 className='title'>Reporte Detallado</h1>
     </div>
   )
 }

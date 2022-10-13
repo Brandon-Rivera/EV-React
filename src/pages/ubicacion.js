45032be1c2cd0b8358c5222d6c2f475ff02c97e9
Frank <franciscotame0@gmail.com>
2022-10-13 02:34:21 -0500

    Jalar ubicacion de usuario específico/ Intento de mapa

--- src/pages/ubicacion.js
+++ src/pages/ubicacion.js
@@ -1,12 +1,55 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useLocation } from "react-router-dom";
+import jwt from "jwt-decode";
+import { GoogleMap, useLoadScript, Marker } from '@react-google-maps/api';
 
 const Ubicacion = () => {
+
     const location = useLocation()
     const userId = location.state
 
-    return(
-        <h1>{userId}</h1>
+    const api = "http://api-vacaciones.us-east-1.elasticbeanstalk.com/api"
+
+    const [ubicacion, setUbicacion] = useState({ city: "" });
+    // vacío, se ejecuta cada vez que renderiza el componente
+    // [], se ejecuta la primera vez que renderiza el componente
+    // [estado], se ejecuta solo cuando se actualice el estado, sin bucle
+    useEffect(() => {
+        getUbicacion()
+    }, [ubicacion])
+
+    /* Todas las funciones */
+    const getUbicacion = async () => {
+        const token = localStorage.getItem('token')
+        const id = jwt(token).id
+        // `${api}/administrador/${id}`
+        const response = await fetch(`${api}/slocationByUser/${userId}`, {
+            headers: {
+                'x-access-token': token
+            }
+        })
+        const data = await response.json()
+        setUbicacion(data)
+    }
+    const ubi = Object.entries(ubicacion)
+
+    const {isLoaded} = useLoadScript({googleMapsApiKey: "AIzaSyBhN9aOoiyE9_ouRyVzQsHRyNVuOpGlAJQ"})
+
+     if(!isLoaded){
+         return <div>Hola</div>
+    }
+
+    return (
+        <div>
+            <h1>{userId}</h1>
+            <GoogleMap
+                zoom={10}
+                center={{lat: 44, lng: -80}}
+                mapContainerClassName="map-container"
+            >
+
+            </GoogleMap>
+        </div>
     )
 }
 

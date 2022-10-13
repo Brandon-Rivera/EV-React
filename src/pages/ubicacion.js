c0ea001fb340caeeb14846c408fb39e91659fcaf
Frank <franciscotame0@gmail.com>
2022-10-13 17:42:45 -0500

    Mapa con marcador listo

--- src/pages/ubicacion.js
+++ src/pages/ubicacion.js
@@ -1,7 +1,9 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useMemo } from "react";
 import { useLocation } from "react-router-dom";
 import jwt from "jwt-decode";
-import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
+import { GoogleMap, LoadScript, MarkerF } from '@react-google-maps/api';
+import './ubicacion.css'
+import {FaMapMarkerAlt} from 'react-icons/fa'
 
 const Ubicacion = () => {
 
@@ -10,13 +12,13 @@ const Ubicacion = () => {
 
   const api = "http://api-vacaciones.us-east-1.elasticbeanstalk.com/api"
 
-  const [ubicacion, setUbicacion] = useState({ city: "" });
+  const [ubicacion, setUbicacion] = useState({lat: 18, lng: -99});
   // vacío, se ejecuta cada vez que renderiza el componente
   // [], se ejecuta la primera vez que renderiza el componente
   // [estado], se ejecuta solo cuando se actualice el estado, sin bucle
   useEffect(() => {
     getUbicacion()
-  }, [ubicacion])
+  }, [])
 
   /* Todas las funciones */
   const getUbicacion = async () => {
@@ -29,33 +31,32 @@ const Ubicacion = () => {
       }
     })
     const data = await response.json()
-    setUbicacion(data)
+    console.log({lat: data[0].lat, lng: data[0].lon})
+    setUbicacion({lat: data[0].lat, lng: data[0].lon})
   }
-  const ubi = Object.values(ubicacion)
 
-  const containerStyle = {
-    width: '70%',
+  const containerStyle = { //PREGUNTAR A HUGO
+    width: '100%',
     height: '500px',
-  };
+    justifyContent: 'center'
+    };
+  
   return (
     <div>
-      <h1>{userId}</h1>
-      {
-        ubi.map(ub => (
-          <LoadScript
+          <LoadScript key={userId}
             googleMapsApiKey="AIzaSyCaZDxJzyD24sCyioMbzBc0vZ66dtjsX_k"
           >
             <GoogleMap
                mapContainerStyle={containerStyle}
-               center={{ lat: ub.lat, lng: ub.lon }}
+               center={ubicacion}
                zoom={17}
              >
-             { /* Child components, such as markers, info windows, etc. */}
-               <></>
+              <MarkerF
+                position={ubicacion}
+              >
+              </MarkerF>
             </GoogleMap>
          </LoadScript>
-        ))
-      }
     </div>
   )
 }

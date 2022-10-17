1afcb1b98e88f049082cd419bc52035fc00a82a1
Brandon-Rivera <A01424338@itesm.mx>
2022-10-16 19:27:58 -0500

    Corregir styled Navbar

--- src/components/Navbar/NavbarElements.js
+++ src/components/Navbar/NavbarElements.js
@@ -5,7 +5,7 @@ import {FaBars} from 'react-icons/fa'
 export const Nav = styled.nav`
     background: #000;
     height: 80px;
-    display: flex;
+    display: flexbox;
     justify-content: space-between;
     padding: 0.5rem calc((100vw - 1000px) / 2);
     z-index: 10;

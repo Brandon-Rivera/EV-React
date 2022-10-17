3fd8445b790fc27a8291b2e8f86c7cc2da4a7b51
Frank <franciscotame0@gmail.com>
2022-10-17 18:17:47 -0500

    centrar general

--- src/pages/general.js
+++ src/pages/general.js
@@ -69,7 +69,7 @@ const General = () => {
       <br></br>
       <table style={{
         border: "solid 1px black",
-        display: "block",
+        display: 'block',
         height: "350px",
         overflow: "auto",
         width: "50%"
@@ -87,11 +87,11 @@ const General = () => {
           {
             userss.map(user => (
               <tr >
-                <td>{user.userName}</td>
-                <td>{user.eMail}</td>
-                <td>{user.phoneNumber}</td>
-                <td>4</td>
-                <td>{user.folio}</td>
+                <td style={{top: 0, textAlign:'center'}}>{user.userName}</td>
+                <td style={{top: 0, textAlign:'center'}}>{user.eMail}</td>
+                <td style={{top: 0, textAlign:'center'}}>{user.phoneNumber}</td>
+                <td style={{top: 0, textAlign:'center'}}>4</td>
+                <td style={{top: 0, textAlign:'center'}}>{user.folio}</td>
               </tr>
             ))}
         </tbody>

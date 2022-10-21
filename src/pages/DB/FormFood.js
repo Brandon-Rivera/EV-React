9bee36dc8b693485c632af8c0480eba3298f5429
Brandon-Rivera <A01424338@tec.mx>
2022-10-21 14:23:54 -0500

    Modificar modal comida

--- src/pages/DB/FormFood.js
+++ src/pages/DB/FormFood.js
@@ -162,7 +162,7 @@ const FormFood = () => {
             <Modal.Header closeButton>
               <Modal.Title>Comida modificada</Modal.Title>
             </Modal.Header>
-            <Modal.Body>Estas a punto de eliminar una comida. ¿Estás seguro?</Modal.Body>
+            <Modal.Body>Woohoo, modificaste a una comida!</Modal.Body>
             <Modal.Footer>
               <Button variant="secondary" onClick={handleClose1}>
                 No
